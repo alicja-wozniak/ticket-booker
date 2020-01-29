0693211d10d352d38dce05ae8488faee82c38fb3
Alicja Woźniak <alicja.wozniak@silksh.pl>
2020-01-29 23:28:16 +0100

    spring boot app init

---
+++ src/test/java/com/alicjawozniak/ticketbooker/TicketBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alicjawozniak.ticketbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
